248f24e9f9c783afc2b578d5a08a50d8d95703ca
sorrychoe <cjssoote@gmail.com>
2023-06-24 15:09:24 +0900

    modified

--- weather.py
+++ weather.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 
 
 def get_weather_data(area: str):
+
+    title = "[오늘의 날씨]"
     url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=' + area + '+날씨'
 
     html = re.get(url)
@@ -13,10 +15,10 @@ def get_weather_data(area: str):
     weather_etc = soup.select_one("div.temperature_info > dl").text.strip()
     weather_etc2 = [etc_info.text.strip() for etc_info in soup.select_one("div.report_card_wrap > ul")][1:-1:2]
 
-    message = temperature + '\n\n' + status + '\n\n' + weather_etc + '\n'
+    message = title + '\n\n' + temperature + ' ' + status + '\n\n' + weather_etc + '\n\n'
 
     for info in weather_etc2:
-        message += info + '\n'
+        message += info + '\n\n'
 
     return message
 
